02fb083f688adfbe89f041d2b77e44e0cb9514ba
Matt Vegh <mdv240@gmail.com>
2020-03-12 23:39:50 -0500

    second hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react"
 
 function App() {
-  const [ answer ] = useState('Yes')
-  console.log(answer)
+  const [answer] = useState('Yes')
+  const [count] = useState(0)
 
   return (
     <div>
       <h1>Is state important to know? {answer}</h1>
+      <h1>{count}</h1>
+      <button>Change!</button>
     </div>
   )
 }
