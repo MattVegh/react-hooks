1f1fc39de0fb3376a14a61a7b1e0f1cd3699dd3f
Matt Vegh <mdv240@gmail.com>
2020-03-12 23:48:51 -0500

    extra practice

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { useState } from "react"
 
 function App() {
-  const [answer] = useState('Yes')
+  const [answer, setAnswer] = useState('Yes')
   const [count, setCount] = useState(0)
 
   function reduce() {
     setCount(count - 1)
   }
 
+  function changeAnswer() {
+    setAnswer(answer === 'Yes' ? 'No ' : 'Yes')
+  }
+
   return (
     <div>
       <h1>Is state important to know? {answer}</h1>
+      <button onClick={changeAnswer}>{answer === 'Yes' ? 'No ' : 'Yes'}</button>
+      
       <h1>{count}</h1>
       <button onClick={() => setCount(count+1)}>Add!</button>
       <button onClick={reduce}>Reduce!</button>
