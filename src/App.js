de2ff499d44da58c398265effb45b2944c53f7e4
Matt Vegh <mdv240@gmail.com>
2020-03-12 23:45:05 -0500

    usestate hook

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ function App() {
   const [answer] = useState('Yes')
   const [count, setCount] = useState(0)
 
+  function reduce() {
+    setCount(count - 1)
+  }
+
   return (
     <div>
       <h1>Is state important to know? {answer}</h1>
       <h1>{count}</h1>
-      <button onClick={() => setCount(count+1)}>Change!</button>
+      <button onClick={() => setCount(count+1)}>Add!</button>
+      <button onClick={reduce}>Reduce!</button>
     </div>
   )
 }
