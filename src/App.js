3b0652ca6791015e3ceb9cb57172c570dea4e42e
Matt Vegh <mdv240@gmail.com>
2020-03-12 23:41:48 -0500

    hook function

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { useState } from "react"
 
 function App() {
   const [answer] = useState('Yes')
-  const [count] = useState(0)
+  const [count, setCount] = useState(0)
 
   return (
     <div>
       <h1>Is state important to know? {answer}</h1>
       <h1>{count}</h1>
-      <button>Change!</button>
+      <button onClick={() => setCount(count+1)}>Change!</button>
     </div>
   )
 }
